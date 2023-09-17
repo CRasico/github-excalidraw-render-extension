fb878f27b98176a9c2e2691bb68eb2a221dc19c8
Cameron Rasico <rasicoca@gmail.com>
2023-09-17 17:10:12 -0400

    more stuffs

--- src/background.ts
+++ src/background.ts
@@ -1,1 +1,20 @@
-console.log(chrome.tabs);
+/// <reference lib="dom" />
+function makeOrange(): void {
+  document.body.style.background = "orange";
+}
+
+chrome.action.onClicked.addListener((tab: chrome.tabs.Tab) => {
+  if (tab == undefined) {
+    return;
+  }
+  var url = tab.url;
+  if (url == undefined || !url.includes(".excalidraw") == false) {
+    return;
+  }
+  console.log("we are on a possible excalidraw file");
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id ? tab.id : -1 },
+    func: makeOrange,
+  });
+});
